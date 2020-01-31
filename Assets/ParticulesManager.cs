0d0f0dcf10b8b64a747dda5fe32b3b2503ad7c18
Coda117 <hugo.kieffer@outlook.com>
2020-02-01 00:18:37 +0100

    Particules Gaz

---
+++ Assets/ParticulesManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gaz : MonoBehaviour
+{
+    public int bouleGazNb;
+    public GameObject g;
+    public bool echanging;
+
+    void Start()
+    {
+        for (int i = 0; i < bouleGazNb; i++)
+        {
+            GameObject bouleGaz = Instantiate(g,new Vector3(transform.position.x + Random.Range(-2,2), transform.position.y + Random.Range(-2, 2),transform.position.z),g.transform.rotation);
+            bouleGaz.transform.parent = transform;
+        }
+
+    }
+
+
+}
