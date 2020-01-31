c8c2456d42efb620c580c93e53a2f7a55e875e91
Coda117 <hugo.kieffer@outlook.com>
2020-02-01 00:44:30 +0100

    Particules Gaz without aled

---
+++ Assets/Script/Particule.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particule : MonoBehaviour
+{
+    public float speed;
+    [HideInInspector]
+    public bool move;
+    public GameObject target;
+    void Update()
+    {
+        if (move == true)
+        {
+           transform.position = Vector2.MoveTowards(transform.position,target.transform.position,speed*Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "zoneEchange")
+        {
+            transform.parent = null;
+            Debug.Log("ok");
+            target = col.gameObject;
+            move = true;
+        }
+    }
+}
---
+++ Assets/Script/ParticulesManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticulesManager : MonoBehaviour
+{
+    public int bouleGazNb;
+    public GameObject particule;
+    public bool echanging;
+
+    void Start()
+    {
+        for (int i = 0; i < bouleGazNb; i++)
+        {
+            GameObject bouleGaz = Instantiate(particule,new Vector3(transform.position.x + Random.Range(-2,2), transform.position.y + Random.Range(-2, 2),transform.position.z),particule.transform.rotation);
+            bouleGaz.transform.parent = transform;
+        }
+
+    }
+
+
+}
