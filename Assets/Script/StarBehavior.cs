338871ed9e129ea2c0320420d0a8d8222ddf3f37
Coda117 <hugo.kieffer@outlook.com>
2020-02-01 08:48:07 +0100

    truc qui marche

--- Assets/Script/StarBehavior.cs
+++ Assets/Script/StarBehavior.cs
@@ -5,36 +5,68 @@ using UnityEngine;
 
 public class StarBehavior : MonoBehaviour {
     [SerializeField] private float starMass = 1;
+    [SerializeField] private float maxStarMass = 1f;
     [SerializeField] private float loseSpeed = 1;
     [SerializeField] private float eatingSpeed = 1;
+    [SerializeField] private float scaleFactor;
 
     [SerializeField] private Color[] colors = new Color[3];
-    private SpriteRenderer sr;
+    public SpriteRenderer sr;
 
     private void Start() {
         sr = this.GetComponent<SpriteRenderer>();
     }
 
-    public IEnumerable EatGas() {
+    public IEnumerator EatGas() {
         float min = 0;
-        while (min < 0) {
-            this.starMass -= Mathf.Lerp(min, 1, Time.deltaTime * this.eatingSpeed);
+        while (min < 1) {
+            if (starMass < maxStarMass)
+            {
+                starMass += Time.deltaTime;
+            }
             min += Time.deltaTime;
             yield return null;
         }
     }
 
     private void Update() {
+        ChangeScale();
         this.LoseMass();
     }
 
+    private void ChangeScale(){
+
+        transform.localScale = Vector3.one * Mathf.Clamp(starMass / scaleFactor, 0, 1) ;
+    }
+
     public void LoseMass() {
-        this.starMass -= loseSpeed * Time.unscaledDeltaTime;
+        if (this.starMass > 0)
+        {
+            this.starMass -= loseSpeed * Time.unscaledDeltaTime;
+        }
+        else
+        {
+            Debug.Log("boom");
+        }
+
     }
 
     public void UpdateColor(Color col) {
         sr.color = col;
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "particle")
+        {
+            Destroy(col.gameObject);
+            if(starMass < maxStarMass)
+            {
+                StartCoroutine(EatGas());
+            }
+            
+        }
     }
 
 }
