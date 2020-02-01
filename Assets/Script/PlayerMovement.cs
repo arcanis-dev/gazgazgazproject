65a21f95dd4f085739e7fc433c3be441947e33c7
Arcanis <arcanis.dev@gmail.com>
2020-02-01 17:41:51 +0100

    Couleurs étoiles

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+[Header("Movement Parameters")]
+
+public float MoveSpeed = 1;
+
+[Range(0,0.30f)]
+public float Acceleration;
+[Range(0,0.30f)]
+public float Decceleration;
+float x_inertia = 0;
+float y_inertia = 0;
+float AxX = 0;
+float AxY = 0;
+
+//code
+
+
+    void FixedUpdate()
+    {
+        AxX = Input.GetAxis("Horizontal");
+        AxY = Input.GetAxis("Vertical");
+
+Debug.Log(AxX);
+        MoveObject();
+    }
+
+    void MoveObject()
+    {
+        if (AxX > 0f)
+        x_inertia = Mathf.Lerp(x_inertia, 1f, Acceleration*Time.deltaTime*60);
+        if (AxX < 0f)
+        x_inertia = Mathf.Lerp(x_inertia, -1f, Acceleration*Time.deltaTime*60);
+        if (Mathf.Abs(AxX) < 0.2f)
+         x_inertia = Mathf.Lerp(x_inertia, 0f, Decceleration *Time.deltaTime*60);
+        
+        if (AxY > 0f)
+        y_inertia = Mathf.Lerp(y_inertia, 1f, Acceleration*Time.deltaTime*60);
+        if (AxY < 0f)
+        y_inertia = Mathf.Lerp(y_inertia, -1f, Acceleration*Time.deltaTime*60);
+        if (Mathf.Abs(AxX) < 0.2f)
+        y_inertia = Mathf.Lerp(y_inertia, 0f, Decceleration *Time.deltaTime*60);
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed* x_inertia,MoveSpeed * y_inertia);
+
+    }
+}
--- Assets/Script/StarBehavior.cs
+++ Assets/Script/StarBehavior.cs
@@ -9,13 +9,13 @@ public class StarBehavior : MonoBehaviour {
     [SerializeField] private float loseSpeed = 1;
     [SerializeField] private float eatingSpeed = 1;
     [SerializeField] private float scaleFactor;
+    
+    [SerializeField] private Gradient sunGradient;
+    [SerializeField] private Color[] colors;
+    
+    public SpriteRenderer starRenderer;
+    public SpriteRenderer FieldRenderer;
 
-    [SerializeField] private Color[] colors = new Color[3];
-    public SpriteRenderer sr;
-
-    private void Start() {
-        sr = this.GetComponent<SpriteRenderer>();
-    }
 
     public IEnumerator EatGas() {
         float min = 0;
@@ -31,12 +31,21 @@ public class StarBehavior : MonoBehaviour {
 
     private void Update() {
         ChangeScale();
+        this.ChangeColor();
         this.LoseMass();
     }
 
     private void ChangeScale(){
-
         transform.localScale = Vector3.one * Mathf.Clamp(starMass / scaleFactor, 0, 1) ;
+    }
+
+    public void ChangeColor() {
+        var alpha = this.FieldRenderer.color.a;
+        this.FieldRenderer.color = this.sunGradient.Evaluate(this.starMass / this.maxStarMass);
+        this.FieldRenderer.color = new Color(this.FieldRenderer.color.r, this.FieldRenderer.color.b, this.FieldRenderer.color.b, alpha);
+        alpha = this.starRenderer.color.a;
+        this.starRenderer.color = this.sunGradient.Evaluate(this.starMass / this.maxStarMass);
+        this.starRenderer.color = new Color(this.starRenderer.color.r, this.starRenderer.color.b, this.starRenderer.color.b, alpha);
     }
 
     public void LoseMass() {
@@ -49,11 +58,6 @@ public class StarBehavior : MonoBehaviour {
             Debug.Log("boom");
         }
 
-    }
-
-    public void UpdateColor(Color col) {
-        sr.color = col;
-        
     }
 
     private void OnTriggerEnter2D(Collider2D col)
