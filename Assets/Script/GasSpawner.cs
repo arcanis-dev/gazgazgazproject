ebb46cbf941cf4d0326f767c89d3f9b87cba4982
Arcanis <arcanis.dev@gmail.com>
2020-02-01 15:09:55 +0100

    Ajouts scrolling soleil

---
+++ Assets/Script/GasSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GasSpawner : MonoBehaviour {
+    private bool isInGameArea;
+    public bool isSpawning = true;
+    [SerializeField] private float spawnTimer = 5f;
+    [SerializeField] private GameObject gas;
+    private void Start() {
+        StartCoroutine(SpawnGasses());
+    }
+
+    private IEnumerator SpawnGasses() {
+        while (isSpawning) {
+            Instantiate(this.gas, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(this.spawnTimer);
+        }
+    }
+}
---
+++ Assets/Script/ScrollSurface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollSurface : MonoBehaviour {
+    public float scrollSpeed = 5f;
+    private Vector2 startPos;
+
+    private void Start() {
+        this.startPos = transform.position;
+    }
+
+    private void Update() {
+        float newPos = Mathf.Repeat(Time.time * this.scrollSpeed, this.GetComponent<SpriteRenderer>().bounds.size.x);
+        transform.position = this.startPos + Vector2.left * newPos;
+    }
+}
